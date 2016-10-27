a26b5a34b6ce687f825657dbd31a71017379c8e2
chaluvadis <surendra.chaluvadi@gmail.com>
2016-10-27 11:11:41 +0400

    Real time movie update is added using socketio

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var http = require('http');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -7,13 +6,14 @@ var app = express();
 
 app.set('PORT', process.env.port || 1337);
 
-//var server = http.createServer(app);
 
 var server = app.listen(app.get('PORT'), function(){
     console.log('Server is running at ' + app.get('PORT'));
 });
 
 var io = require('socket.io').listen(server);
+
+app.io = io;
 
 var index = require('./routes/index');
 var admin = require('./routes/admin');
@@ -28,11 +28,3 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', index);
 app.use('/admin', admin);
-
-//emit some events to the client
-io.on('connection', function(socket){
-    socket.emit('news', {hello: 'world'});
-    socket.on('event from client', function(data){
-        console.log('from client :', data);
-    })
-});
--- routes/admin.js
+++ routes/admin.js
@@ -17,19 +17,29 @@ var storage = multer.diskStorage({
 });
 
 router.get('/', function (req, res) {
+    var list = list.filterBy()
     res.render('admin', { movies: list });
 });
 
-router.post('/', multer({ storage : storage }).single('logo'), function (req, res) {
-    list.push({
+router.post('/', multer({ storage: storage }).single('logo'), function (req, res) {
+
+    var data = {
         title: req.body.title,
         director: req.body.director,
         genre: req.body.genre,
         logo: '/images/' + (list.length + 1) + '.jpg'
-    });
+    }
+
+    list.push(data);
     fs.writeFileSync('list.json', JSON.stringify(list));
+    data.log = "http://localhost:1337" + data.logo;
+    //emit some events to the client
+    req.app.io.emit('movie', data);
+
+    req.app.io.on('movie-updated', function (data) {
+        console.log('movie updated in client :', data);
+    })
     res.send('movie uploaded successfully');
 });
 
 module.exports = router;
-
